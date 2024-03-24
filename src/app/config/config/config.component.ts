3fce1c1471a4902d74cd5a8466b4524d96ac9146
ricardosantanadev <ricardosantanadev@gmail.com>
2024-03-24 13:13:05 -0300

    example1-simple-angular-table-conexao-backend-httpclient-passando-o-retorno-do-metodo-get-para-o-observable

---
+++ src/app/config/config/config.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Config } from '../models/config';
+import { ConfigService } from '../services/config.service';
+
+@Component({
+  selector: 'app-config',
+  templateUrl: './config.component.html',
+  styleUrls: ['./config.component.scss']
+})
+export class ConfigComponent {
+  config!: Observable<Config[]>;
+  displayedColumns: string[] = ['heroesUrl', 'textfile', 'date'];
+
+  constructor(private configService: ConfigService) {
+    this.showConfig();
+  }
+
+  showConfig() {
+    this.config = this.configService.getConfig();
+  }
+}
