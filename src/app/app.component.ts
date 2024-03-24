dc7aae419b53edcf3bfcf1fab270a899b0d09301
ricardosantanadev <ricardosantanadev@gmail.com>
2024-03-24 12:31:06 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'example1-simple-angular-table-conexao-backend-httpclient-passando-o-retorno-do-metodo-get-para-o-observable';
+}
